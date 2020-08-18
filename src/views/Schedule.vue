00489e6c30288fb7912a8f4aad5278bfcbce52da
IkemotoNanako <chibencaizi@gmail.com>
2020-08-18 12:36:11 +0900

    commment3

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,5 +1,36 @@
 <template>
-<div>
+<div v-if="hoge">
+    <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class="bg bg3"></div>
+    <header>
+        <h1>うちで学校！！</h1>
+    </header>
+    <h2>時間割</h2>
+    <ol class="subject">
+        <li v-for="(subject) in subjects" :key="subject">{{subject}}</li>
+    </ol>
+    <input type="text" v-model="newSubject">
+    <input type="submit" value="決定" @click="addSubject">
+    <input type="submit" value="削除" @click="delateSubject">
+    <div>
+      <p>勉強時間</p>
+      <p><input type="number" placeholder="勉強時間" v-model="study">分</p>
+      <p>休憩時間</p>
+      <p><input type="number" placeholder="休憩時間" v-model="rest">分</p>
+    </div>
+    <div id="timer">
+      <div class="timer left">
+        <div class="time">
+            {{ formatTime }}
+        </div>
+        <button v-on:click="start" v-if="!timerOn">Start</button>
+        <button v-on:click="stop" v-if="timerOn">Stop</button>
+      </div>
+    </div>
+    <audio src="../assets/Japanese_School_Bell02-01.mp3" autoplay controls v-if="chime"></audio>
+</div>
+<div v-else>
     <div class="bg"></div>
     <div class="bg bg2"></div>
     <div class="bg bg3"></div>
@@ -42,10 +73,11 @@ export default {
         rest: '',//記入された休憩時間
         min: 0,//初期の分
         sec: 0,//初期の秒
-        timerOn: false,
-        timerObj: null,
+        timerOn: false,//タイマーのON/OFFのため
+        timerObj: null,//setTimeoutを格納して、timerを止める時使う
         chime: false,//trueの時チャイム音を鳴らすものをfalseにしておく
         number:0,//勉強時間と休憩時間の切り替えのための変数
+        hoge: '',//レスポンシブ対応のための変数
         subjects:[]
     }
 },
@@ -57,22 +89,24 @@ methods: {
     },
   //時間割を消す記述 
     delateSubject() {
-        this.subjects.splice(this.newSubject);
+        this.subjects.pop(this.newSubject);
     },
   //タイマーを動かす記述
     count() {
       if (this.sec <= 0 && this.min >= 1) {
         this.min --;
         this.sec = 59;
-      } else if (this.sec <= 0 && this.min <= 0 && this.number % 2 == 0)//numberが偶数の時
+      } else if (this.sec <= 0 && this.min <= 0 
+      && this.number % 2 == 0)//numberが偶数の時
       {
-        this.chime = true//チャイムを鳴らす//
+        this.chime = true//チャイムを鳴らす
         this.number++;//勉強時間と休憩時間の切り替えのための変数numberを＋１する
         this.min = this.study;//記入された勉強時間を代入
         this.sec = 0;
-      } else if (this.sec <= 0 && this.min <= 0 && this.number % 2 == 1)//numberが奇数の時
+      } else if (this.sec <= 0 && this.min <= 0 
+      && this.number % 2 == 1)//numberが奇数の時
       {
-        this.chime = true;//チャイムを鳴らす//
+        this.chime = true;//チャイムを鳴らす
         this.number++;//勉強時間と休憩時間の切り替えのための変数numberを＋１する
         this.min = this.rest;//記入された休憩時間を代入
         this.sec = 0;
@@ -83,19 +117,20 @@ methods: {
         this.sec --;
       }
     },
-    start: function() {
+    start() {
       let self = this;
       this.timerObj = setInterval(function() {self.count()}, 1000)
-      this.timerOn = true; //timerがOFFであることを状態として保持
+      this.timerOn = true; //timerをONにする
     },
 
-    stop: function() {
+    stop() {
       clearInterval(this.timerObj);
-      this.timerOn = false; //timerがOFFであることを状態として保持
+      this.timerOn = false; //timerをOFFにする
     },
   },
+  //分数・秒数が一桁台であれば 01という風に表示する
   computed: {
-    formatTime: function() {
+    formatTime() {
       let timeStrings = [
         this.min.toString(),
         this.sec.toString()
@@ -107,6 +142,14 @@ methods: {
         }
       })
       return timeStrings[0] + ":" + timeStrings[1]
+    }
+  },
+  //レスポンシブ対応のための記述
+  created() {
+    if (screen.width >= 480) {
+      this.hoge = true;
+    } else {
+      this.hoge = false;
     }
   }
 }
@@ -149,7 +192,6 @@ methods: {
 header {
     margin: 0px;
     box-sizing: border-box;
-    padding: 0px 50px;
     display: flex;
 }
 h1 {
@@ -158,15 +200,8 @@ h1 {
     font-weight: bold;
     padding: 20px;
     text-shadow: 2px 2px 3px #333;
-}
-.menu {
-    padding: 0 20px;
-    cursor: pointer;
-}
-a {
-    text-decoration: none;
-    color:#fff;
-    padding-left: 200px;
+    align-items: center;
+    justify-content: center;
 }
 /*時間割*/
 h2 {
@@ -188,7 +223,13 @@ h2 {
 .time {
   font-size: 80px;
 }
-
+.left {
+  position: absolute;
+  top: 70%;
+}
+p {
+  font-size: 30px;
+}
 /*レスポンシブ対応*/
 @media screen and (max-width: 768px) {
     h1 {
@@ -198,6 +239,9 @@ h2 {
     }
     h2 {
         font-size: 17px;
+    }
+    p  {
+        font-size: 15px;
     }
 }
 
